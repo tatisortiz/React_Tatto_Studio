0bf7d322433530fdb653bf6048da57156852bb8b
tatianaD <angietatiana167@gmail.com>
2024-07-23 14:35:42 +0200

    feat: create home

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+
+import { NavLink, useNavigate } from "react-router-dom";
+import "./Header.css"
+
+
+export const Header = () => {
+	
+
+	return (
+		<>
+		
+		<NavLink to = "/register">Register</NavLink>
+		<NavLink to = "/">Home</NavLink>
+	
+			
+				
+			
+		</>
+	);
+};
---
+++ src/views/Register/Register.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import "./Register.css";
+
+export const Register = () => {
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleChange(e) {
+    console.log("Handle Change");
+
+    setCredentials((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  console.log(credentials);
+
+  return (
+  
+      <div>
+        <h1>Register</h1>
+        <h2>Create an account or login</h2>
+
+        <input
+          type="text"
+          name="email"
+          id=""
+          placeholder="Email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          id=""
+          placeholder="Password"
+          onChange={handleChange}
+        />
+        <input type="button" value="Register" />
+      </div>
+  );
+};
